d52b444789195c4c508acc02fc28b16f9e5aabb1
Christos Papadopoulos <christos.papadopoulos@student.hepl.be>
2021-11-05 13:41:59 +0100

    Fin du labo 5

--- BLOC1/Bases de la programmation en assembleur/Les Entiers et les Flottants/Labo 5/Labo 5/main.c
+++ BLOC1/Bases de la programmation en assembleur/Les Entiers et les Flottants/Labo 5/Labo 5/main.c
@@ -115,6 +115,122 @@ int main()
 		movsd d, xmm0
 	}
 	*/
+	
+	/* i = (int)(-d + 2.81 - -b * f + 3.5) / 3; // i = 701
+	const double dconst = -1;
+	const double d281 = 2.81;
+	const double d35 = 3.5;
+	_asm
+	{	
+		movsd xmm0, d
+		movsd xmm1, dconst
+		mulsd xmm0, xmm1
 
+		movsx eax, b
+		neg eax
+		movss xmm1, f
+		cvtsi2ss xmm2, eax
+		mulss xmm1, xmm2
+		
+		addsd xmm0, d281
+		cvtss2sd xmm1, xmm1
+		subsd xmm0, xmm1
+		addsd xmm0, d35
+		cvttsd2si eax, xmm0
 
+		mov ebx, 3
+		cdq
+		idiv ebx
+		mov i, eax
+	}
+	*/
+	
+	/* d = (int)d * 3 + (double)-i / f + (b + 2.1); // d = 50.7190476190...
+	const double d21 = 2.1;
+	const double dconst = -1;
+	_asm
+	{	
+		// d'abord la parenthèse
+		movsx eax, b
+		movsd xmm0, d21
+		cvtsi2sd xmm1, eax
+		addsd xmm0, xmm1
+
+		// casting et signe
+		cvttsd2si eax, d
+		cvtsi2sd xmm1, i
+		mulsd xmm1, dconst
+
+		// Operations
+		imul eax, 3
+		cvtss2sd xmm2, f
+		divsd xmm1, xmm2
+		cvtsi2sd xmm2, eax
+		addsd xmm1, xmm2
+		addsd xmm1, xmm0
+		movsd d, xmm1
+	}
+	*/
+	/* b = (f + 0.5 * i) / b + f; // b = 54
+	const double d05 = 0.5;
+	_asm
+	{
+		movsd xmm0, d05
+		cvtsi2sd xmm1, i
+		mulsd xmm0, xmm1
+
+		cvtss2sd xmm1, f
+		addsd xmm0, xmm1
+
+		cvtsi2sd xmm1, b
+		divsd xmm0, xmm1
+
+		cvtss2sd xmm1, f
+		addsd xmm0, xmm1
+		cvttsd2si eax, xmm0
+		mov b, al
+		
+	}
+	*/
+	/* f = d / 2 * i / 3 + b / 2.5; // f = 26.4666672
+	const double d25 = 2.5;
+	_asm
+	{
+		movsd xmm0, d
+		mov eax, 2
+		cvtsi2sd xmm1, eax
+		divsd xmm0, xmm1
+		cvtsi2sd xmm1, i
+		mulsd xmm0, xmm1
+		mov eax, 3
+		cvtsi2sd xmm1, eax
+		divsd xmm0, xmm1
+
+		movsx eax, b
+		cvtsi2sd xmm1, eax
+		divsd xmm1, d25
+		addsd xmm0, xmm1
+		cvtsd2ss xmm0, xmm0
+		movss f, xmm0
+
+	}
+	*/
+	/* f = (d * 100) / (b - 0xff & 0xf0); // f = 9.8125
+	_asm
+	{
+		mov eax, 100
+		cvtsi2sd xmm0, eax
+		mulsd xmm0, d
+
+		movsx eax, b
+		sub eax, 0xff
+		and eax, 0xf0
+
+		cvtsi2sd xmm1, eax
+		divsd xmm0, xmm1
+
+		cvtsd2ss xmm0, xmm0
+		movss f, xmm0
+	}
+	*/
 }
