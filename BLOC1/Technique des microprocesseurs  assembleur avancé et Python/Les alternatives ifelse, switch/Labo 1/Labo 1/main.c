b8bc29a5ffebbc6c295b7b8b311390663aa0bce6
Christos Papadopoulos <christos.papadopoulos@student.hepl.be>
2022-02-04 19:08:27 +0100

    Ajout du deuxième exercice

--- BLOC1/Technique des microprocesseurs  assembleur avancé et Python/Les alternatives ifelse, switch/Labo 1/Labo 1/main.c
+++ BLOC1/Technique des microprocesseurs  assembleur avancé et Python/Les alternatives ifelse, switch/Labo 1/Labo 1/main.c
@@ -59,3 +59,61 @@ void main()
  * vitesse moyenne du trajet (par exemple, pour un trajet de 200 km effectué en 2 heures et 30
  * minutes, on a une vitesse moyenne de 80 km/h).
  */
+
+/*
+const char msg[] = "Entrez la distance en kilometres : ";
+const char msg1[] = "Entrez la duree du trajet en heures et minutes : ";
+const char printFormatdistance[] = "%d";
+const char printFormatDuree[] = "%d %d";
+const char msg2[] = "\nLa vitesse moyenne est de : %d km/h";
+
+int distance, heures, minutes;
+
+void main()
+{
+	_asm
+	{
+		push offset msg
+		call dword ptr printf
+		add esp, 4
+
+		lea eax, distance
+		push eax
+		push offset printFormatdistance
+		call dword ptr scanf
+		add esp, 8
+
+		push offset msg1
+		call dword ptr printf
+		add esp, 4
+
+		lea eax, minutes
+		push eax
+		lea eax, heures
+		push eax
+		push offset printFormatDuree
+		call dword ptr scanf
+		add esp,12
+
+		// Conversion des valeurs en double
+		cvtsi2sd xmm0, heures
+		cvtsi2sd xmm1, minutes
+		mov eax, 60
+		cvtsi2sd xmm2, eax
+		divsd xmm1, xmm2
+		addsd xmm0, xmm1
+
+		cvtsi2sd xmm1, distance
+		divsd xmm1, xmm0
+
+		cvttsd2si eax, xmm1
+
+		push eax
+		push offset msg2
+		call dword ptr printf
+		add esp, 8
+
+	}
+
+}
+*/
