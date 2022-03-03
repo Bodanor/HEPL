08036496e096c77918bcb75549abfb049618978d
Christos Papadopoulos <christos.papadopoulos@student.hepl.be>
2022-03-03 19:29:17 +0100

    Ajoutout du Labo 3

---
+++ BLOC1/Q2/Technique des microprocesseurs  assembleur avancé et Python/Les boucles for, while et les caractères et les chaînes de caractères/asm/Labo 3/Labo 3/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 1. Afficher le contenu d’un vecteur de doubles.
+
+double vect[5] = { 5.0, 3.4, 98.3764, 874.847, 437.874 };
+const char printFormat[] = "%lf ";
+int i;
+
+i = 0;
+int main()
+{
+	_asm
+	{
+	debutwhile:
+		cmp i, 5
+		jge finwhile	// Si i >= a 5 alors on saute
+		
+		mov eax, i		//Eax stocke I
+		mov ebx, offset vect	// EBX contient l'adresse de vect double
+		movsd xmm0, qword ptr[ebx + eax*8]	//XMM0, ebx(adresse) + eax(i) + 8 (double = 8 octets)
+		sub esp, 8	// On ne peux pas push xmm0, donc on soustrait 8 a esp
+		movsd qword ptr[esp], xmm0	// On deplace xmm0 a esp
+		push offset printFormat		// on place le format pour printf dans la pile
+		call dword ptr printf		// call printf
+		add esp, 12		// Rajouter 12 a esp, pour détruire le push (xmm0 + formatprintf)
+			
+		inc i	// Incrementer i a chaque boucle
+		jmp debutwhile
+	finwhile:
+
+	}
+
+}
+*/
+
+/* 2. Copier dans un vecteur de doubles uniquement les valeurs négatives figurant dans un
+vecteur d’entiers (par exemple, si VecEntiers contient [-9, 4, 8, -2, -7, 5], alors on aura dans
+VecDoubles [-9, -2, -7]). */
+
+/*
+int vecentiers[6] = {-9, 4, 8, -2, -7, 5};
+dounle vecdouble[6];
+int main()
+{
+
+}
+*/
