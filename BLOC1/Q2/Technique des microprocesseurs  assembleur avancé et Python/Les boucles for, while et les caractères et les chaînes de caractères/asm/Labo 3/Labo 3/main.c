8dc18ab5b7e903cefd2756cb5e6eac5e3f7d2fe1
Christos Papadopoulos <christos.papadopoulos@student.hepl.be>
2022-03-03 20:47:46 +0100

    Ajout de commentaires à l'ex 2

--- BLOC1/Q2/Technique des microprocesseurs  assembleur avancé et Python/Les boucles for, while et les caractères et les chaînes de caractères/asm/Labo 3/Labo 3/main.c
+++ BLOC1/Q2/Technique des microprocesseurs  assembleur avancé et Python/Les boucles for, while et les caractères et les chaînes de caractères/asm/Labo 3/Labo 3/main.c
@@ -45,28 +45,49 @@ int main()
 {
 	_asm
 	{
-		nop
 	debutwhile:
-		mov eax, i
-		cmp eax, 6
+		mov eax, i	// EAX, contient i (compteur jusque 6)
+		cmp eax, 6	// Si eax, >= 6 alors on saute
 		jge finwhile
 
-		mov ebx, offset vecentiers
-		cmp dword ptr[ebx + eax*4], 0
-		jl ajouterdouble
-		inc i
-		jmp debutwhile
+		mov ebx, offset vecentiers	// EBX, adresse vecentiers
+		cmp dword ptr[ebx + eax*4], 0	// EBX(adresse) + eax(i) + 4(car des entiers), compare à 0
+		jl ajouterdouble		// Si plus petit que zero (donc negatif) alors on saute
+		inc i	// Sinon in incremente i et donc on passe a l'entier suivant
+		jmp debutwhile	// Rvenir au debut de boucle while
 
-	ajouterdouble:
-		cvtsi2sd xmm0, qword ptr[ebx + eax*4]
-		mov eax, b
-		mov ebx, offset vecdouble
-		movsd qword ptr[ebx + eax*8], xmm0
-		inc b
-		inc i
-		jmp debutwhile
+	ajouterdouble:	// Si on est ici c'est qu'on a détécté un nombre négatif
+		cvtsi2sd xmm0, qword ptr[ebx + eax*4]	// Convertion du nmb négatif en double dans XMM0
+		mov eax, b	// On prend la valeur b (variable qui sert de compteur dans le vecteur de double)
+		mov ebx, offset vecdouble	//EBX contient l'adresse du vecdouble
+		movsd qword ptr[ebx + eax*8], xmm0	// Deplacement de XMM0, dans le vecteur de double (8 car double)
+		inc b	// Ne pas oublier d'incrementer b
+		inc i	// Pareil ici puisque après on jump au debut de boucle 
+		jmp debutwhile	// Jump début de boucle
 
-	finwhile:
+	finwhile:	// Sert à sortir de boucle
 	}
+}
+*/
+
+/* 3. Insérer une valeur saisie au clavier au bon endroit dans un vecteur d’entiers dont les valeurs
+sont déjà ordonnées dans un ordre croissant (par exemple, si VecEntiers contient [2, 8, 11,
+15, 20] et que la valeur à insérer est 10, alors on aura dans VecEntiers [2, 8, 10, 11, 15, 20]).
+
+int VecEntiers[5] = { 2, 8, 11, 15, 20 };
+int valeur;
+char scanfFormat[] = "%d";
+void main()
+{
+	_asm
+	{
+		mov eax, offset valeur
+		push eax
+		push offset scanfFormat
+
+
+	}
+
+
 }
 */
