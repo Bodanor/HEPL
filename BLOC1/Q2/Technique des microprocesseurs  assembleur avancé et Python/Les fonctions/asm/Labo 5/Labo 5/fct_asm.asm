d58542719cf8103df6a7e08f152be076f64ddb82
Christos Papadopoulos <christos.papadopoulos@student.hepl.be>
2022-04-17 18:57:41 +0200

    Fin du labo 5

--- BLOC1/Q2/Technique des microprocesseurs  assembleur avancé et Python/Les fonctions/asm/Labo 5/Labo 5/fct_asm.asm
+++ BLOC1/Q2/Technique des microprocesseurs  assembleur avancé et Python/Les fonctions/asm/Labo 5/Labo 5/fct_asm.asm
@@ -54,4 +54,117 @@ PerimetreCercle PROC
 
 PerimetreCercle ENDP
 
+TesterPythagore PROC
+	cotea EQU <DWORD PTR[ESP + 4]>
+	coteb EQU <DWORD PTR[ESP + 8]>
+	cotec EQU <DWORD PTR[ESP + 12]>
+	
+	mov eax, cotea
+	cmp eax, coteb
+
+	jna tryb
+	
+	cmp eax, cotec
+	jna tryc
+	
+	imul eax, eax
+	mov ebx, coteb
+	imul ebx, ebx
+	mov ecx, cotec
+	imul ecx, ecx
+	add ebx, ecx
+	cmp eax, ebx
+	je rectTrue
+	jmp rectFalse
+
+
+tryb:
+	mov eax, coteb
+	cmp eax, cotec
+	jna tryc
+	
+	imul eax, eax
+	mov ebx, cotea
+	imul ebx, ebx
+	mov ecx, cotec
+	imul ecx, ecx
+	add ebx, ecx
+	cmp eax, ebx
+	je rectTrue
+	jmp rectFalse
+
+tryc:
+	mov eax, cotec
+
+	imul eax, eax
+	mov ebx, coteb
+	imul ebx, ebx
+	mov ecx, cotea
+	imul ecx, ecx
+	add ebx, ecx
+	cmp eax, ebx
+	je rectTrue
+	jmp rectFalse
+
+rectTrue:
+	mov eax, 1
+	ret
+	
+rectFalse:
+	mov eax, 0
+	ret
+
+
+
+
+TesterPythagore ENDP
+
+Calculer PROC
+val1 EQU <QWORD PTR[ESP + 12]>
+val2 EQU <QWORD PTR[ESP + 20]>
+operation EQU <DWORD PTR[ESP + 28]>
+tmp EQU <QWORD PTR[ESP + 0]>
+
+sub esp, 8
+
+movsd xmm0, val1
+movsd xmm1, val2
+
+
+cmp operation, 1
+je addition
+
+cmp operation, 2
+je soustraction
+
+cmp operation, 3
+je multiplication
+
+	divsd xmm0, xmm1
+	movsd tmp, xmm0
+	jmp return_statement
+
+addition:
+	addsd xmm0, xmm1
+	movsd tmp, xmm0
+	jmp return_statement
+
+soustraction:
+	subsd xmm0, xmm1
+	movsd tmp, xmm0
+	jmp return_statement
+
+
+multiplication:
+	mulsd xmm0, xmm1
+	movsd tmp, xmm0
+
+return_statement:
+	fld tmp
+	add esp, 8
+	ret
+
+Calculer ENDP
+
+
 END
--- BLOC1/Q2/Technique des microprocesseurs  assembleur avancé et Python/Les fonctions/asm/Labo 5/Labo 5/main.c
+++ BLOC1/Q2/Technique des microprocesseurs  assembleur avancé et Python/Les fonctions/asm/Labo 5/Labo 5/main.c
@@ -3,10 +3,13 @@
 extern int ConvertToFahr(int celsius);
 extern int LoiOhm(int resistance, int intensite);
 extern double PerimetreCercle(double rayon);
+extern  int TesterPythagore(int cotea, int coteb, int cotec);
+double Calculer(double val1, double val2, int operation);
+
 void main()
 {
-	double d;
+	double a;
+	a = Calculer(40.0, 30.0, 4);
 
-	d = PerimetreCercle(150.9);
 
 }
