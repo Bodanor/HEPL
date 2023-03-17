d7bd3f48698e95022ec701955268441784d22c3e
Bodanor <papadopoulos.chris@icloud.com>
2023-03-17 19:50:03 +0100

    Modification Labo2 Automation

--- BLOC2/Q2/Automatisation/Labo/Labo2/main.c
+++ BLOC2/Q2/Automatisation/Labo/Labo2/main.c
@@ -62,7 +62,6 @@ union
 } u_capt;
 
 void printSuiviPiece(void);
-void *FctCapteur(void *);
 void* FctMouvement(void *);
 void* FctAffichage(void*);	
 
@@ -71,7 +70,6 @@ int fd_petra_in, fd_petra_out;
 
 main()
 {
-	pthread_t thread_capteur;
 	pthread_t thread_mvmt;
 	pthread_t thread_affichage;
 	
@@ -109,35 +107,13 @@ main()
 		perror("Erreur de creation du thread de lecture de capteurs !\n");
 		return -1;
 	}
-	ret = pthread_create(&thread_capteur, NULL, FctCapteur, NULL);
-	if (ret != 0) {
-		perror("Erreur de creation du thread de lecture de capteurs !\n");
-		return -1;
-	}
-	ret = pthread_create(&thread_affichage, NULL, FctAffichage, NULL);
-	if (ret != 0) {
-		perror("Erreur de creation du thread de lecture de capteurs !\n");
-		return -1;
-	}
 
-	
 	ret = pthread_join(thread_mvmt, NULL);
 	if (ret != 0) {
 		perror("Erreur de synchronisation de thread !\n");
 		return -1;
 	}
 	
-	ret = pthread_join(thread_capteur, NULL);
-	if (ret != 0) {
-		perror("Erreur de synchronisation de thread !\n");
-		return -1;
-	}
-
-	ret = pthread_join(thread_affichage, NULL);
-	if (ret != 0) {
-		perror("Erreur de synchronisation de thread !\n");
-		return -1;
-	}
 }
 
 
@@ -167,138 +143,153 @@ void printSuiviPiece(void)
 	fflush(stdout);
 }
 
-void* FctAffichage(void *p1)
-{
-	printf("\033[2J"); // Clear screen
-	printf("\033[0;0H"); // Workaround car les printf continuent après les logs, donc il suffit de placer le curseur au début du terminal !
-	printf("\e[?25l"); // Permet de cacher le curseur
-	printSuiviPiece();
-	while (1)
-	{
-		if (u_capt.capt.L1)
-		{
-			printf("\033[1;35H");
-			printf("*");
-		}
-		else
-		{
-			printf("\033[1;35H");
-			printf(" ");
-
-		}
-
-		if (u_capt.capt.L2)
-		{
-			printf("\033[8;35H");
-			printf("*");
-		}
-		else
-		{
-			printf("\033[8;35H");
-			printf(" ");
-
-		}
-
-		if (u_capt.capt.L2)
-		{
-			printf("\033[8;35H");
-			printf("*");
-		}
-		else
-		{
-			printf("\033[8;35H");
-			printf(" ");
-
-		}
-
-		u_capt.capt.PP = 1;
-		if (u_capt.capt.PP)
-		{
-			printf("\033[0;35H");
-			printf("*");
-		}
-		else
-		{
-			printf("\033[0;35H");
-			printf(" ");
-		}
-
-		if (u_capt.capt.T)
-		{
-			printf("\033[5;35H");
-			printf("*");
-		}
-		else
-		{
-			printf("\033[5;35H");
-			printf(" ");
-		}
-
-	}
-}
 
 void* FctMouvement(void* p)
 {
-	
-/* 	struct ACTUATEURS
-{
-	unsigned CP : 2; // Carrier Position
-	unsigned C1 : 1; // Conveyor 1
-	unsigned C2 : 1;
-	unsigned PV : 1; // Plunger Vacuum
-	unsigned PA : 1; // Plunger Activate
-	unsigned AA : 1; // Arm Activate
-	unsigned GA : 1; // Grapple Activate
-};
-union
-{
-	struct ACTUATEURS act;
-	unsigned char byte;
-} u_act; */
-// 	struct CAPTEURS
-// {
-// 	unsigned L1 : 1; // Length 1
-// 	unsigned L2 : 1; // Length 2
-// 	unsigned T : 1;	 /* cabl*/
-// 	unsigned S : 1;	 // Slot
-// 	unsigned CS : 1; // Carrier Stable
-// 	unsigned AP : 1; // Arm Position
-// 	unsigned PP : 1; // Plunger Position
-// 	unsigned DE : 1; // Dispenser Empty
-// 					 /*                 unsigned H1 : 1;
-// 									  unsigned H2 : 1;
-// 									  unsigned H3 : 1;
-// 									  unsigned H4 : 1;
-// 									  unsigned H5 : 1;
-// 									  unsigned H6 : 1;
-// 									  unsigned    : 2;	*/
-// };
 
-// union
-// {
-// 	struct CAPTEURS capt;
-// 	unsigned char byte;
-// } u_capt;
 	int rc, compteur_slot;
 	struct CAPTEURS previous_capteurs_struct;
 	unsigned char previous_capteurs_byte;
+	
+
+	read(fd_petra_in, &u_capt.byte, 1);
 
 	while (1)
 	{
-		compteur_slot = 0;
+
+		compteur_slot = 0; // Pour plus tard pour l'etat pour slot
+
 		previous_capteurs_byte = u_capt.byte;
 		/* Tant que l'etat des capteurs a pas change, on ne fait rien */
-		while (previous_capteurs_byte != u_capt.byte)
+		u_act.act.PV = 0;		
+		u_act.act.CP = 0; // Le plongeur est en position 0 initialement
+		rc = write(fd_petra_out, &u_act.byte, 1);
+		if (rc <= 0)
 		{
-			if(u_capt.capt.L1 != previous_capteurs_struct.L1)
+			perror("Erreur d'ecriture : ");
 		}
+
+		read(fd_petra_in, &u_capt.byte, 1);
+
+		/* Tant que pas au debut on attend*/
+		while(u_capt.capt.CS == 1)
+			;
+		
+
+		read(fd_petra_in, &u_capt.byte, 1);
+
+		if (u_capt.capt.DE == 0)
+		{
+			u_act.act.PA = 1;
+			rc = write(fd_petra_out, &u_act.byte, 1);
+			if (rc <= 0)
+			{
+				perror("Erreur d'ecriture : ");
+			}
+			sleep(2);
+				
+			u_act.act.PV = 1;
+			rc = write(fd_petra_out, &u_act.byte, 1);
+			if (rc <= 0)
+			{
+				perror("Erreur d'ecriture : ");
+			}
 			
+			u_act.act.PA = 0;
+			rc = write(fd_petra_out, &u_act.byte, 1);
+			if (rc <= 0)
+			{
+				perror("Erreur d'ecriture : ");
+			}
+			
+			read(fd_petra_in, &u_capt.byte, 1);
+			while (u_capt.capt.PP == 1){
+					read(fd_petra_in, &u_capt.byte, 1);
+					}
+						
+			u_act.act.CP = 1;
+			rc = write(fd_petra_out, &u_act.byte, 1);
+			if (rc <= 0)
+			{
+				perror("Erreur d'ecriture : ");
+			}
+			sleep(1); // Teporisation pour capteur
+			read(fd_petra_in, &u_capt.byte, 1);
+			/* Tant que en mouvement, on fait rien */
+			while(u_capt.capt.CS == 1)
+					read(fd_petra_in, &u_capt.byte, 1);
+					
+			/* On lache la piece sur le convoyeur 1*/
+			u_act.act.PV = 0;
+			u_act.act.C1 = 1;
+			rc = write(fd_petra_out, &u_act.byte, 1);
+			if (rc <= 0)
+			{
+				perror("Erreur d'ecriture : ");
+			}
+
+			read(fd_petra_in, &u_capt.byte, 1);
+			while (u_capt.capt.S == 0)
+				read(fd_petra_in, &u_capt.byte, 1);
+
+			/* Piece au debut du capteur S a partir d'ici*/
+
+			read(fd_petra_in, &u_capt.byte, 1);
+			while (u_capt.capt.S == 1) {
+				read(fd_petra_in, &u_capt.byte, 1);
+				nanosleep(10000);
+				while (u_capt.capt.S == 0 && compteur_slot != 1)
+				{
+					compteur_slot = 1;
+				}
+			}
+
+			int finis = 0;
+			while (!finis) {
+				read(fd_petra_in, &u_capt.byte, 1);
+
+				if (u_capt.capt.S == 0) 
+				{
+					// Demarrage timer
+					while( !finis)
+					{
+
+					}
+					read(fd_petra_in, &u_capt.byte, 1);
+					if (u_capt.capt.S == 1)
+					{
+						compteur_slot = 1;
+					}
+					else
+						compteur_slot = 0;
+				}
+			}
+
+			/*
+while (previous_capteurs_byte != u_capt.byte)
+{
+
+	if (u_capt.capt.S != previous_capteurs_struct.S)
+	{
+
+	}
+
+	if(u_capt.capt.L1 != previous_capteurs_struct.L1)
+		;
+	// A completer
+}
+*/
+
+
+		}
+		
 
 		/* Si un capteur change, on regarde lequel a changer d'etat */
 
 		
+		/*
 		u_act.byte = 0; // Debut du tour de boucle
-		previous_capteurs. = 0
+		previous_capteurs_byte = 0
 
 		
 		u_act.act.CP = 0; // Le plongeur est en position 0
@@ -380,22 +371,9 @@ union
 		{
 			perror("Erreur d'ecriture : ");
 		}
+		*/
 
 
 	}
 
-}
-void *FctCapteur(void * p)
-{
-	int rd;
-	
-	while (1)
-	{
-		rd = read(fd_petra_in, &u_capt.byte, 1);
-		if(rd <=0)
-		{
-			perror("Erreur de read : ");
-			exit(1);
-		}
-	}
 }
